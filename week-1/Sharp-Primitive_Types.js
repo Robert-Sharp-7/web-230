b0a5b42102ddc365f6cb0db46142624837ab89d0
Robert Sharp <robert.sharp.dev@gmail.com>
2017-12-02 16:20:17 -0600

    Adding Sharp-Primitive_Types.js

---
+++ week-1/Sharp-Primitive_Types.js
@@ -0,0 +1,28 @@
+// start program
+
+/*
+    Expected output:
+    
+    First name
+    Last name
+    Age
+    Street
+    City
+    State 
+    Zip code 
+*/ 
+
+// variable declaration and assignment goes here ...
+var firstName="Robert";
+var lastName="Sharp";
+var age="24";
+var street="221B Baker St";
+var city="London";
+var state="England";
+var zipCode="01801";
+
+// output 
+console.log('\n'); 
+
+
+// end program
