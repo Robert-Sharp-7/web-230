5e65ca8a5439ce516c2e4570e3afba47d2b3171c
Robert Sharp <robert.sharp.dev@gmail.com>
2017-12-03 23:57:05 -0600

    Adding Assignment 1.5

---
+++ week-1/Sharp-Types_Values_Variables.js
@@ -0,0 +1,40 @@
+var myFirstName = "Robert";
+var myLastName = "Sharp";
+var todaysDate = new Date().toLocaleDateString();
+var assignmentNum = "Assignment 1.5";
+
+var programHeader = "\n" + myFirstName + " " + myLastName + "\n"
+    + assignmentNum + "\nDate: " + todaysDate;
+
+console.log(programHeader);
+console.log("\n"); 
+//
+var firstName="Richard"
+var lastName="Banks"
+var address="568 Huntington Rd"
+var payRate var n.toFixed(2)="950"
+var hireDate = new Date(2016, 6, 15);
+
+var firstName="Thomas"
+var lastName="Phillips"
+var address="16 Gale St"
+var payRate var n.toFixed(2)="950"
+var hireDate = new Date(2016, 6, 15);
+
+var firstName="Anna"
+var lastName="Welsh"
+var address="2122 E Harney St"
+var payRate var n.toFixed(2)="950"
+var hireDate = new Date(2016, 6, 15);
+
+var firstName="Emily"
+var lastName="Fitzpatrick"
+var address="424 Jones St"
+var payRate var n.toFixed(2)="950"
+var hireDate = new Date(2016, 6, 15);
+
+var firstName="Christopher"
+var lastName="Lawson"
+var address="103 Sandburg Place"
+var payRate var n.toFixed(2)="950"
+var hireDate = new Date(2016, 6, 15);
