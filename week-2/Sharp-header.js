401fa89f14b5f4cef327fd9d7b94cfa974b4bd82
Robert Sharp <robert.sharp.dev@gmail.com>
2017-12-10 11:23:30 -0600

    Adding header assignments and renamed files

---
+++ week-2/Sharp-header.js
@@ -0,0 +1,7 @@
+var header = require('../header.js');
+
+console.log("\nTesting the header.js file's output..."); 
+
+console.log(header.display("Robert", "Sharp", "Exercise 2.2")); 
+console.log("\n");
+
